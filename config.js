aea28ea784382292d26834e7c9c6933134613129
Love1234564 <158076084+Love1234564@users.noreply.github.com>
2024-01-31 09:55:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['8066642758', 'Dinuwa Official²⁰²³', true],
+  ['8066642758', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
